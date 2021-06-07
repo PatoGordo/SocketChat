b15ea503e9bdacc986ab7f60fa4d57d48fcc8598
PatoGordo <59514690+PatoGordo@users.noreply.github.com>
2021-06-07 15:49:29 -0300

    bugs fixed, comments, backend code updated now the server is ruinning on the heroku

--- backend/index.js
+++ backend/index.js
@@ -3,23 +3,35 @@ const io = require('socket.io')(http, {
   cors: {origin: "*"}
 })
 
+// This array||db store all messages sent
 var messages = []
 
 io.on('connection', (socket) => {
   console.log(`New connection [${socket.id}]`)
 
+  // Send all messages for new users
   messages.forEach(msg => {
     socket.emit('message', msg)
   })
 
   socket.on('message', (message) => {
-    // console.log(message)
+
+    // Check for invalid possibilities
     if(message.text === '' || message.author === '' || message.author === 'SYSTEM') {
       return
     }
 
-    if(message.text.substr(0, 5).toLowerCase() === 'clear') {
+    // Send socket id to user  
+    if(message.text.substr(0, 5).toLowerCase() === '!myid') {
+      socket.emit('message', {text: `(Message sent only for you) Your id is: ${socket.id}`, author: 'SYSTEM'})
+      return
+    }
+
+    // Clear all chat
+    if(message.text.substr(0, 6).toLowerCase() === '!clear') {
+
       messages = []
+
       setTimeout(() => {
         io.emit('message', {text: 'all messages have been deleted', author: 'SYSTEM'})
         messages.push({text: 'all messages have been deleted', author: 'SYSTEM'})
@@ -27,9 +39,15 @@ io.on('connection', (socket) => {
       return
     }
 
+    // Add messages to message array||db and emit foi all usets
     messages.push(message)
     io.emit('message', message)
   })
+
 })
 
-http.listen(8080, () => console.log('SERVER STARTED'))
+const port = process.env.PORT || 3000
+
+http.listen(port, () => {
+  console.log('SERVER STARTED')
+})
